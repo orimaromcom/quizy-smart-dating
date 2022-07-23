2a04708f2cf2833d961354f880afedfb09db4342
Ori Marom <ori.marom10@gmail.com>
2022-07-23 11:49:59 +0300

    running server

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const logger = require("./server/middleware/logger");
+
+const app = express();
+
+ app.get("/", (req, res) => {
+  res.status(200).json({
+    health: `ok`,
+  });
+});  
+
+app.use(logger);
+
+const port = process.env.PORT || "8080";
+
+app.listen(port, function () {
+  console.log("Running on " + port);
+});
+
+module.exports = app;
