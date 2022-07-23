62efbd30d8ca90f6237ce9bc1f1bf48408de6453
Tal Livneh <talivneh8@gmail.com>
2022-07-23 13:19:48 +0300

    fix typo after PR

---
+++ src/client/src/routes/QuizPage.jsx
@@ -0,0 +1,10 @@
+import Page from "../components/Page/Page";
+import Quiz from "../components/Quiz/Quiz";
+
+export default function QuizPage() {
+  return (
+    <Page>
+      <Quiz />
+    </Page>
+  );
+}
