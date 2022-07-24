4489b34eeb4f37bce29155f508b1eb005b8a46fb
Tal Livneh <50097959+talivneh@users.noreply.github.com>
2022-07-24 16:59:35 +0300

    Add files via upload

---
+++ src/client/src/components/Brainmates/Brainmates.jsx
@@ -0,0 +1,18 @@
+import "./brainmates.css";
+import MateCard from "./MateCard/MateCard";
+
+export default function Brainmates() {
+  return (
+    <div className="brain-mates-container">
+      <MateCard />
+      <MateCard />
+      <MateCard />
+      <MateCard />
+      <MateCard />
+      <MateCard />
+      <MateCard />
+      <MateCard />
+      <MateCard />
+    </div>
+  );
+}
