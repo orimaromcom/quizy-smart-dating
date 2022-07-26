109ace3fb18ab3151f7e682e0e11e9dc53d4f4e7
Ori Marom <ori.marom10@gmail.com>
2022-07-26 13:55:37 +0300

    wip get questions shuffle options

--- src/server/server-quiz/clients/trivia-client.js
+++ src/server/server-quiz/clients/trivia-client.js
@@ -3,25 +3,26 @@ const { default: axios } = require("axios");
 const URL_EXAMPLE =
   "https://opentdb.com/api.php?amount=10&category=26&difficulty=easy";
 //const url = "https://opentdb.com";
-const FILM_CATEGORY_NUMBER = 11;
+const FILM_CATEGORY_NUMBER = "11";
+const SPORTS_CATEGORY_NUMBER = "21";
+const COMPUTERS_CATEGORY_NUMBER = "18";
+const CELEBRITIES_CATEGORY_NUMBER = "26";
+const HISTORY_CATEGORY_NUMBER = "23";
+const MUSIC_CATEGORY_NUMBER = "12";
 
 async function fetchQuestions() {
   const errorResponse = {
     error: true,
-
     description: `Error fetching questions`,
   };
   try {
     const response = await axios.get(URL_EXAMPLE).then((response) => response);
-    const questions = response.data;
+    const questions = response.data.results;
 
-    if (questions) {
-      return {
-        error: false,
-        questions: questions,
-        description: `Pokemon with ID ${pokemonId} data`,
-      };
-    }
+    return {
+      error: false,
+      questions: questions,
+    };
   } catch (error) {
     return errorResponse;
   }
--- src/server/server-quiz/services/quiz-manager.js
+++ src/server/server-quiz/services/quiz-manager.js
@@ -1,8 +1,41 @@
 const { Question } = require("../../db/models");
+const triviaClient = require("../clients/trivia-client");
 
 async function getAllQuestions() {
-  const questions = await Question.findAll();
+  const personalQuestions = await Question.findAll();
+
+  const triviaQuestionsResponse = await triviaClient.fetchQuestions();
+  const quizyTriviaQuestions = changeQuestionsStructure(
+    triviaQuestionsResponse.questions
+  );
+
+  const questions = { ...personalQuestions, ...quizyTriviaQuestions };
   return questions;
+}
+
+function changeQuestionsStructure(triviaQuestions) {
+  const newQuestionsArray = []
+  for (let question of triviaQuestions) {
+    const newQuestion = {"id" : null, "type" : "trivia", "topic" : `${question.category}`, "amountOfOptions" : `${question.type}`}
+    
+    const options = question.incorrect_answers
+    options.push(question.correct_answer)
+    shuffleOptions(options)
+    console.log(options)
+   
+    newQuestionsArray.push(newQuestion)
+  }
+}
+
+function shuffleOptions (optionsArray){
+  for (var i = optionsArray.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = optionsArray[i];
+    optionsArray[i] = optionsArray[j];
+    optionsArray[j] = temp;
+}
+
+
 }
 
 module.exports = {
