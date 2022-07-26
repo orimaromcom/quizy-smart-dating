085b2d6d5a8306506de29231230734334c522829
Ori Marom <ori.marom10@gmail.com>
2022-07-26 14:01:04 +0300

    quizy question structure

--- src/server/server-quiz/services/quiz-manager.js
+++ src/server/server-quiz/services/quiz-manager.js
@@ -14,28 +14,43 @@ async function getAllQuestions() {
 }
 
 function changeQuestionsStructure(triviaQuestions) {
-  const newQuestionsArray = []
+  const newQuestionsArray = [];
   for (let question of triviaQuestions) {
-    const newQuestion = {"id" : null, "type" : "trivia", "topic" : `${question.category}`, "amountOfOptions" : `${question.type}`}
-    
-    const options = question.incorrect_answers
-    options.push(question.correct_answer)
-    shuffleOptions(options)
-    console.log(options)
-   
-    newQuestionsArray.push(newQuestion)
+    const newQuestion = {
+      id: null,
+      type: "trivia",
+      topic: `${question.category}`,
+      amountOfOptions: `${question.type}`,
+    };
+
+    const options = question.incorrect_answers;
+    options.push(question.correct_answer);
+    shuffleOptions(options);
+
+    if (question.type === "multiple") {
+      newQuestion.option1 = options[0];
+      newQuestion.option2 = options[1];
+      newQuestion.option3 = options[2];
+      newQuestion.option4 = options[3];
+    }
+
+    if (question.type === "boolean") {
+      newQuestion.option1 = options[0];
+      newQuestion.option2 = options[1];
+    }
+
+    newQuestion.correctOption = question.correct_answer;
+    newQuestionsArray.push(newQuestion);
   }
 }
 
-function shuffleOptions (optionsArray){
+function shuffleOptions(optionsArray) {
   for (var i = optionsArray.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
     var temp = optionsArray[i];
     optionsArray[i] = optionsArray[j];
     optionsArray[j] = temp;
-}
-
-
+  }
 }
 
 module.exports = {
