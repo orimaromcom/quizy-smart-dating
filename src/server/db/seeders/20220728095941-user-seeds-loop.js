e6e1d27a79f2bb1225e6de714c4f00b486c5b615
Anna Shishkova <asshishkova@gmail.com>
2022-07-28 14:16:11 +0300

    Seed 500 random users

---
+++ src/server/db/seeders/20220728095941-user-seeds-loop.js
@@ -0,0 +1,44 @@
+const faker = require('faker');
+
+'use strict';
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const CITIES = ["Bat Yam", "Givatayim", "Haifa", "Herzliya", "Holon", "Nes Ziona", "Netanya", "Petah Tikva", "Raanana", "Ramat-Gan", "Ramla", "Rehovot", "Rishon Lezion", "Tel Aviv", "Yavne"];
+const amountOfCities = CITIES.length;
+const GENDERS = ["male", "female", "other"]
+const amountOfGenders = GENDERS.length;
+const RELATIONS = ["romantic", "friends"]
+const amountOfRalations = RELATIONS.length;
+
+const usersJSON = [];
+   for(let i = 0; i < 500; i++){
+      usersJSON.push({
+        username: `${faker.name.firstName()}${randomInt(1, 100)}`,
+        phoneNumber: faker.phone.phoneNumber('+972 5# ### ## ##'),
+        password: "123456",
+        age: randomInt(20, 40),
+        location: CITIES[randomInt(0, amountOfCities)],
+        radius: randomInt(0, 50),
+        gender: GENDERS[randomInt(0, amountOfGenders)],
+        lookingForGender: GENDERS[randomInt(0, amountOfGenders )],
+        lookingForRelationsType: RELATIONS[randomInt(0, amountOfRalations)],
+        lookingForMinAge: randomInt(20, 29),
+        lookingForMaxAge: randomInt(30, 40),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+   }
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {}); // delete existing
+    await queryInterface.bulkInsert('Users', usersJSON,{});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkDelete('Users', null, {});
+  }
+};
