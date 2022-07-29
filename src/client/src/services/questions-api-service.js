73babf55cf2a0171eaacc384231c6f331a21b92f
Ori Marom <ori.marom10@gmail.com>
2022-07-29 10:02:39 +0300

    wip move to axios

--- src/client/src/services/questions-api-service.js
+++ src/client/src/services/questions-api-service.js
@@ -4,7 +4,7 @@ export default class QuestionsApiService {
   static async getQuestions() {
     try {
       const response = await axios.get(`/quiz/questions`);
-      const questions = response.data;s
+      const questions = response.data;
       return questions;
     } catch (error) {
       throw new Error(error.message);
