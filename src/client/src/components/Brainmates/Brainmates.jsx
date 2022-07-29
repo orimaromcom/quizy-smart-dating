3eb66b9fbdcbf1655d682c948c075c17929aede4
Tal Livneh <talivneh8@gmail.com>
2022-07-29 12:53:39 +0300

    different cards for matching and pending

--- src/client/src/components/Brainmates/Brainmates.jsx
+++ src/client/src/components/Brainmates/Brainmates.jsx
@@ -10,21 +10,29 @@ export default function Brainmates({ brainmates, fetcBrainmates }) {
 
   return (
     <div className="brain-mates-container">
-      <MateCard status={"match"} userName={"brainmate.userName"} />
-      {/* {Object.keys(brainmates.likeBack).map((brainmate, i) => {
+      {Object.keys(brainmates.likeBack).map((brainmate, i) => {
+        const current = brainmates.likeBack[brainmate];
         return (
           <MateCard
             key={`match` + i}
             status={"match"}
-            userName={"brainmate.userName"}
+            userName={current.username}
+            imgSrc={current.picture}
+            achievements={current.bestResultDescription}
           />
         );
       })}
       {Object.keys(brainmates.pending).map((brainmate, i) => {
+        const current = brainmates.pending[brainmate];
         return (
-          <MateCard key={`pending` + i} status={"pending"} userName={""} />
+          <MateCard
+            key={`pending` + i}
+            status={"pending"}
+            userName={current.username}
+            achievements={current.bestResultDescription}
+          />
         );
-      })} */}
+      })}
     </div>
   );
 }
--- src/client/src/components/Brainmates/MateCard/MateCard.jsx
+++ src/client/src/components/Brainmates/MateCard/MateCard.jsx
@@ -1,19 +1,24 @@
 import "./MateCard.css";
 import mateAvatar from "../../../images/avatar_img.png";
 
-export default function MateCard({ status, img, userName }) {
+export default function MateCard({ status, imgSrc, userName, achievements }) {
+  // <Badge badgeContent={100} color="secondary">
+  //   <MailIcon />
+  // </Badge>
+
   return (
-    <div className="mate-card">
+    <div className={`mate-card ` + status}>
+      {status === "match" ? <span>Match!</span> : null}
       <div className="card-avatar-container">
         <img
           className="card-avatar-img"
-          src={status === "match" ? img : mateAvatar}
+          src={status === "match" ? imgSrc : mateAvatar}
           alt="mate"
         />
       </div>
       <div className="card-content">
         <h1>{userName}</h1>
-        <p>The best in sports question in Tel Aviv!</p>
+        <p>{achievements}!</p>
       </div>
     </div>
   );
