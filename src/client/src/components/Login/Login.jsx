5be0a61f971666f7e2defd69b2963f6d40701db5
Anna Shishkova <asshishkova@gmail.com>
2022-07-30 22:54:00 +0300

    Log in with admin password

--- src/client/src/components/Login/Login.jsx
+++ src/client/src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from "react-router-dom";
+import { useCallback, useState } from "react";
 import UserApiService from "../../services/user-api-service";
 import jwt_decode from "jwt-decode";
 
@@ -11,7 +12,6 @@ export default function Login({
   const navigate = useNavigate();
   async function getGoogleLoginData(credentialResponse) {
     const {email, picture, name} = jwt_decode(credentialResponse.credential);
-    console.log('email', email);
     const userInfo = await UserApiService.getUserByEmail(email);
     if (userInfo.id === null) {
       userInfo.picture = picture;
@@ -20,20 +20,62 @@ export default function Login({
     setUserAction(userInfo);
   }
 
+
   function goToProfilePage() {
     navigate("/profile");
   }
 
+  const GoogleLogIn = <GoogleLogin
+                        onSuccess={getGoogleLoginData}
+                        onError={() => {
+                          console.log('Login failed');
+                        }}
+                      />
+
+  const [testEmail, setTestEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const getTesterLoginData = useCallback(async (event) => {
+    event.preventDefault();
+    const testUserEmail = testEmail;
+    setTestEmail("");
+    const secretPass = pass;
+    setPass("");
+    const userInfo = await UserApiService.getUserByEmail(testUserEmail);
+    if (userInfo.id === null || secretPass !== process.env.REACT_APP_TESTING_PASSWORD) {
+      console.log('Error, invalid data');
+    } else {
+      setUserAction(userInfo);
+    }
+  },[testEmail, pass, setUserAction]);
+
+  const TesterLogin = <form onSubmit={getTesterLoginData}>
+                        <label>
+                          User email
+                          <input type="text" name="email"
+                            value={testEmail} onChange={(e) => setTestEmail(e.target.value)}/>
+                        </label>
+                        <br />
+                        <label>
+                          Password
+                          <input type="password" name="password"
+                            value={pass} onChange={(e) => setPass(e.target.value)}/>
+                        </label>
+                        <br />
+                        <input type="submit" value="Log in" />
+                      </form>
+
+
   return (
     <div>
-      <h1>Login</h1>
       { !userEmail &&
-        <GoogleLogin
-          onSuccess={getGoogleLoginData}
-          onError={() => {
-            console.log('Login failed');
-          }}
-        />
+        <div>
+          <h1>Google Login</h1>
+          { GoogleLogIn }
+          <br /><br /><br />
+          <h1>Tester Login</h1>
+          { TesterLogin }
+        </div>
       }
       {
         userEmail && userId &&
@@ -49,8 +91,8 @@ export default function Login({
         <div>
           <h1>You are signing up</h1>
           <br />
-          <p>You alredy have email: {userEmail}</p>
-          <p>But your user id is: null {userId}</p>
+          <p>You already have email: {userEmail}</p>
+          <p>But your user id is not defined: null {userId}</p>
           <br />
           <button onClick={goToProfilePage}>Go to profile page</button>
           <br /> <br />
@@ -59,19 +101,13 @@ export default function Login({
           <p>userName: {userName}</p>
           <p>userPicture: {userPicture.slice(0,20)}</p>
           <p>(From the server they will come like null, but we can replace them with the info from the state)</p>
+          <p>(And we also need to create a raw for Trivia answers!)</p>
           <br /> <br />
         </div>
       }{
         userEmail &&
         <button onClick={resetUserAction}>Log out</button>
       }
-
-      <br /><br /><br />
-      <h1>Tester login</h1>
-      <form action="">
-        user email: <input type="text" /> <br />
-        password: <input type="text" />
-      </form>
     </div>
   )
 }
