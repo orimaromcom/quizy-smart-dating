5e513030d6f57f56e03062981024ac7da05909de
Ori Marom <ori.marom10@gmail.com>
2022-07-30 23:50:29 +0300

    clean lines

--- src/client/src/components/Quiz/ProgressBar/ProgressBar.jsx
+++ src/client/src/components/Quiz/ProgressBar/ProgressBar.jsx
@@ -1,24 +1,22 @@
 import "./progress-bar.css";
 import { Box } from "@mui/material";
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import LinearProgress, { linearProgressClasses } from "@mui/material/LinearProgress";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 
 import React, { useState } from "react";
 
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+const BorderLinearProgress = styled(LinearProgress)(() => ({
   height: 15,
   borderRadius: 5,
 
   [`& .${linearProgressClasses.bar}`]: {
     borderRadius: 5,
-    
   },
 }));
 
 export default function ProgressBar({ progressPercentage }) {
- 
   return (
     <Box className="progress-bar-container">
       <BorderLinearProgress
@@ -26,10 +24,8 @@ export default function ProgressBar({ progressPercentage }) {
         variant="determinate"
         value={progressPercentage || 0}
         height={10}
-       
       />
       <FavoriteIcon className="heart-icon" color={"primary"} sx={{ fontSize: 70 }} />
-     
     </Box>
   );
 }
