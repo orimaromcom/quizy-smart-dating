f5de0fa2e867553acb48c239a86fc3306d38fb5b
Ori Marom <ori.marom10@gmail.com>
2022-07-30 11:31:29 +0300

    add heart loader

--- src/client/src/components/Quiz/Heart/HeartLoader.jsx
+++ src/client/src/components/Quiz/Heart/HeartLoader.jsx
@@ -1,12 +1,12 @@
 import style from "./heart.module.scss";
 import heart_v2_style from "./heart-v2.module.scss";
 
-export default function Heart({ quote }) {
+export default function HeartLoader({  }) {
   return (
     <div className={style.container}>
       <div className={style.heart_container}>
-        { /*<div className={style.heart} />*/ }
-{ 
+        {/* <div className={style.heart} /> */}
+
         <div className={heart_v2_style.icon}>
           <svg
             className={heart_v2_style["heart-main"]}
@@ -24,12 +24,10 @@ export default function Heart({ quote }) {
           >
             <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z" />
           </svg>
-        </div> }
+        </div>
       </div>
 
       <div className={style.text}>
-        <div className={style.quote}>{quote?.quote}</div>
-        <div className={style.author}>{quote?.author}</div>
       </div>
     </div>
   );
