84feb4a463f8607c6b7a48e98e5233fe1081dc58
Tal Livneh <talivneh8@gmail.com>
2022-07-30 08:49:15 +0300

    working version

--- src/client/src/components/Brainmates/Brainmates.jsx
+++ src/client/src/components/Brainmates/Brainmates.jsx
@@ -5,34 +5,38 @@ import MateCard from "./MateCard/MateCard";
 export default function Brainmates({ brainmates, fetcBrainmates }) {
   //temp: we should load globaly
   useEffect(() => {
-    if (!!brainmates.length) fetcBrainmates(2);
+    if (!brainmates.length) fetcBrainmates(2);
   }, []);
 
   return (
     <div className="brain-mates-container">
-      {Object.keys(brainmates.likeBack).map((brainmate, i) => {
-        const current = brainmates.likeBack[brainmate];
-        return (
-          <MateCard
-            key={`match` + i}
-            status={"match"}
-            userName={current.username}
-            imgSrc={current.picture}
-            achievements={current.bestResultDescription}
-          />
-        );
-      })}
-      {Object.keys(brainmates.pending).map((brainmate, i) => {
-        const current = brainmates.pending[brainmate];
-        return (
-          <MateCard
-            key={`pending` + i}
-            status={"pending"}
-            userName={current.username}
-            achievements={current.bestResultDescription}
-          />
-        );
-      })}
+      {brainmates.likeBack
+        ? Object.keys(brainmates.likeBack).map((brainmate, i) => {
+            const current = brainmates.likeBack[brainmate];
+            return (
+              <MateCard
+                key={`match` + i}
+                status={"match"}
+                userName={current.username}
+                imgSrc={current.picture}
+                achievements={current.bestResultDescription}
+              />
+            );
+          })
+        : null}
+      {brainmates.pending
+        ? Object.keys(brainmates.pending).map((brainmate, i) => {
+            const current = brainmates.pending[brainmate];
+            return (
+              <MateCard
+                key={`pending` + i}
+                status={"pending"}
+                userName={current.username}
+                achievements={current.bestResultDescription}
+              />
+            );
+          })
+        : null}
     </div>
   );
 }
