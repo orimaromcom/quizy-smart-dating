ed44893797cef0ee94c743b0e8f1199394316f0d
Tal Livneh <talivneh8@gmail.com>
2022-07-30 10:07:08 +0300

    before PR

--- src/client/src/components/Quiz/quiz-connector.js
+++ src/client/src/components/Quiz/quiz-connector.js
@@ -1,23 +1,34 @@
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import Quiz from "./Quiz";
-import { getAllQuestions, getQuestionIndex } from "../../redux/selectors/questions-entities-selectors";
+import {
+  getAllQuestions,
+  getQuestionIndex,
+} from "../../redux/selectors/questions-entities-selectors";
 import { fetchNewQuestions } from "../../redux/actions/fetch-questions-action";
-import {incrementQuestionIndex} from "../../redux/actions/increment-questions-index-actions"
-import { getAllAnswers } from "../../redux/selectors/answers-entities-selector"
+import { incrementQuestionIndex } from "../../redux/actions/increment-questions-index-actions";
+import { getAllAnswers } from "../../redux/selectors/answers-entities-selector";
 import { addAnswer } from "../../redux/actions/add-answer-action";
-import {incrementAnswersIndex} from "../../redux/actions/increment-answers-index-action"
+import { incrementAnswersIndex } from "../../redux/actions/increment-answers-index-action";
 
 const mapStateToProps = (state) => {
   const questions = getAllQuestions(state);
-  const questionIndex = getQuestionIndex(state)
-  const answersArray = getAllAnswers(state)
+  const questionIndex = getQuestionIndex(state);
+  const answersArray = getAllAnswers(state);
   const MOCK_USER_ID = 1;
-  return { questions, MOCK_USER_ID ,answersArray, questionIndex};
+  return { questions, MOCK_USER_ID, answersArray, questionIndex };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ fetchNewQuestions, addAnswer, incrementQuestionIndex, incrementAnswersIndex }, dispatch);
+  return bindActionCreators(
+    {
+      fetchNewQuestions,
+      addAnswer,
+      incrementQuestionIndex,
+      incrementAnswersIndex,
+    },
+    dispatch
+  );
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
