ab3a6a671d9d676e10da11c00ca6b3f38430cbb2
Ori Marom <ori.marom10@gmail.com>
2022-07-31 10:12:20 +0300

    userbar init

--- src/client/src/components/Layout/Layout.jsx
+++ src/client/src/components/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../Navbar/Navbar";
+import ResponsiveAppBar from "../Userbar/Userbar"
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
+     <ResponsiveAppBar/>
       <Outlet />
       <Navbar />
     </>
