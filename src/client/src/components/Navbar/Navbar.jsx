3edcfa1860f2d9c0c0bddd07025159154d70d0f0
Ori Marom <ori.marom10@gmail.com>
2022-08-01 17:09:21 +0300

    wip nav colors

--- src/client/src/components/Navbar/Navbar.jsx
+++ src/client/src/components/Navbar/Navbar.jsx
@@ -9,10 +9,9 @@ import PersonIcon from "@mui/icons-material/Person";
 export default function Navbar({ pageButtonValue, updatePageButtonAction }) {
   const currentPage = window.location.href.split("/").pop();
   updatePageButtonAction(currentPage);
-  console.log(pageButtonValue === "login")
 
   return (
-    (pageButtonValue === "login" ? null : (<Box>
+    /* pageButtonValue === "login" ? null : */ <Box>
       <BottomNavigation
         showLabels
         value={pageButtonValue}
@@ -21,30 +20,34 @@ export default function Navbar({ pageButtonValue, updatePageButtonAction }) {
         }}
       >
         <BottomNavigationAction
+          id="Mui-selected-quiz"
           component={Link}
           value="quiz"
           to="/quiz"
           icon={<PsychologyIcon sx={{ fontSize: 45 }} />}
         />
         <BottomNavigationAction
+          id="Mui-selected-brainmates"
           component={Link}
           value="brainmates"
           to="/brainmates"
           icon={<FavoriteIcon sx={{ fontSize: 45 }} />}
         />
         <BottomNavigationAction
+          id="Mui-selected-achievements"
           component={Link}
           value="achievements"
           to="/achievements"
           icon={<EmojiEventsIcon sx={{ fontSize: 45 }} />}
         />
         <BottomNavigationAction
+        id="Mui-selected-profile"
           component={Link}
           value="profile"
           to="/profile"
           icon={<PersonIcon sx={{ fontSize: 45 }} />}
         />
       </BottomNavigation>
-    </Box>) )
+    </Box>
   );
 }
