f1b1bfe95399744167a444e2cacf3ed7687856a6
Anna Shishkova <asshishkova@gmail.com>
2022-08-04 13:35:48 +0300

    Add auth to getSuggestionsForUser

--- src/server/server-matching/routes/matching-router.js
+++ src/server/server-matching/routes/matching-router.js
@@ -22,7 +22,7 @@ matchingRouter.get('/trivia-answers/:id', auth, getUserTriviaAnswers);
 matchingRouter.get('/achievements/:id', auth, getUserAchievements);
 matchingRouter.get('/distances', auth, getAllDistances);
 matchingRouter.get('/distances/:id', auth, getUserDistances);
-matchingRouter.get('/suggestions/:id', getSuggestionsForUser);
+matchingRouter.get('/suggestions/:id', auth, getSuggestionsForUser);
 
 matchingRouter.post('/postdistances/:id', auth, postUserDistances);
 matchingRouter.post('/postdistances', auth, postAllUsersDistances);
