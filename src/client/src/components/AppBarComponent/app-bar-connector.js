24d7f54b7a8fb9512026e35c0ed82b1660e45deb
Tal Livneh <talivneh8@gmail.com>
2022-08-04 19:39:00 +0300

    wip

--- src/client/src/components/AppBarComponent/app-bar-connector.js
+++ src/client/src/components/AppBarComponent/app-bar-connector.js
@@ -2,11 +2,12 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import AppBarComponent from "./AppBarComponent";
 import { userLogoutAction } from "../../redux/actions/user-logout-action";
-
 import { getProfile } from "../../redux/selectors/profile-entity-selector";
+// import { getCorrectAnswers } from "../../redux/selectors/answers-entities-selector";
 
 const mapStateToProps = (state) => {
-  const totalScore = 213;
+  // const totalScore = getCorrectAnswers();
+  const totalScore = 219;
   const profile = getProfile(state);
   return { profile, totalScore };
 };
