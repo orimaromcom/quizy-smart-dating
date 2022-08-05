d7450de601ac11fbea173a927d0682590c121907
Anna Shishkova <asshishkova@gmail.com>
2022-08-05 18:13:44 +0300

    Add distances and answers request to loader action

--- src/client/src/redux/reducers/app-view-reducer.js
+++ src/client/src/redux/reducers/app-view-reducer.js
@@ -17,7 +17,9 @@ const appViewReducer = (state = initialState, action) => {
 
     // case actionTypes.FETCH_BRAINMATES_REQUEST:
     // case actionTypes.FETCH_ACHIEVEMENTS_REQUEST:
-    // case actionTypes.FETCH_SUGGESTIONS_REQUEST:
+    case actionTypes.FETCH_SUGGESTIONS_REQUEST:
+    case actionTypes.POST_DISTANCES_FAILURE:
+    case actionTypes.POST_ANSWERS_REQUEST:
     case actionTypes.UPDATE_PROFILE_REQUEST:
     case actionTypes.FETCH_QUESTIONS_REQUEST: {
       return {
