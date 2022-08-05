8831736bedcb193a06629a8b9097b15b09a883dc
Anna Shishkova <asshishkova@gmail.com>
2022-08-05 10:06:12 +0300

    Navigate from / to /quiz

--- src/client/src/components/Layout/Layout.jsx
+++ src/client/src/components/Layout/Layout.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavbarConnector from "../Navbar/navbar-connector";
 import AppBar from "../AppBarComponent/app-bar-connector";
-import { Outlet } from "react-router-dom";
 import ToasterConnector from "../Toaster/toaster-connector";
 
+
+
+
 export default function Layout() {
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/quiz");
+    }
+  }, []);
   return (
     <>
       <AppBar />
